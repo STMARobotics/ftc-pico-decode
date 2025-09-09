b9a093b471229a717d42ad9343b76cd54bac4e70
STMA Robotics <robotics@stmarobotics.org>
2025-09-09 17:51:19 -0500

    template

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DRIVE.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Basic Mecanum Drive", group = "Linear OpMode")
+public class DRIVE extends LinearOpMode {
+    // variables
+    @Override
+    public void runOpMode() {
+        // initialization section
+        waitForStart();
+        while (opModeIsActive()) {
+        // do stuff here its where you want to be doing stuff
+
+        }
+    }
+}
