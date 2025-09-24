223c460df7e57b9f0374b1650f10f96c06e04305
STMA Robotics <robotics@stmarobotics.org>
2025-09-23 19:59:28 -0500

    9/23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DRIVE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DRIVE.java
@@ -4,17 +4,44 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
 @TeleOp(name = "Basic Mecanum Drive", group = "Linear OpMode")
 public class DRIVE extends LinearOpMode {
     // variables
-    @Override
-    public void runOpMode() {
-        // initialization section
-        waitForStart();
-        while (opModeIsActive()) {
-        // do stuff here its where you want to be doing stuff
 
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // initialization
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+//        DcMotor turret1 =hardwareMap.dcMotor.get("turret1");
+//        DcMotor turret2 =hardwareMap.dcMotor.get("turret2");
+
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+            // action area
+            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y;
+            double rx = gamepad1.right_stick_x;
+            double t = gamepad1.right_trigger;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontRightPower = (y + x + rx) / denominator;
+            double rearRightPower = (y - x + rx) / denominator;
+            double rearLeftPower = (y + x - rx) / denominator;
+            double frontLeftPower = (y - x - rx) / denominator;
+
+            //turret1.setPower(t);
+            //turret2.setPower(t);
+
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(rearRightPower);
+            backLeft.setPower(rearLeftPower);
+            frontLeft.setPower(frontLeftPower);
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class auto extends LinearOpMode {
+    // Declare variables
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized. Waiting for start...");
+        telemetry.update();
+        waitForStart();
+        if (isStopRequested()) return;
+
+
+
+        //stuff i think
+        moveDrivetrain(frontLeft, backLeft, frontRight, backRight, 0.5, 1000);
+        stopDrivetrain(frontLeft, backLeft, frontRight, backRight);
+        rotateDrivetrainLeft(frontLeft, backLeft, frontRight, backRight, 0.5, 1000);
+        stopDrivetrain(frontLeft, backLeft, frontRight, backRight);
+
+    }
+
+    private void moveDrivetrain(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight, double power, int duration) throws InterruptedException {
+        frontLeft.setPower(power);
+        backLeft.setPower(power);
+        frontRight.setPower(power);
+        backRight.setPower(power);
+        sleep(duration);
+    }
+
+    private void stopDrivetrain(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight) {
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+    }
+
+    private void rotateDrivetrainLeft(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight, double power, int duration) throws InterruptedException {
+        frontLeft.setPower(-power);
+        backLeft.setPower(-power);
+        frontRight.setPower(power);
+        backRight.setPower(power);
+        sleep(duration);
+    }
+
+    private void rotateDrivetrainRight(DcMotor frontLeft, DcMotor backLeft, DcMotor frontRight, DcMotor backRight, double power, int duration) throws InterruptedException {
+        frontLeft.setPower(power);
+        backLeft.setPower(power);
+        frontRight.setPower(-power);
+        backRight.setPower(-power);
+        sleep(duration);
+
+    }
+
+
+}
